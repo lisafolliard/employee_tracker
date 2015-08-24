7711cfe514e5827996e5ca26062d9e41c6cd3245
JuliannaGreen & LisaFolliard <student@epicodus.com>
2015-08-24 14:15:27 -0700

    add ability update division name

--- app.rb
+++ app.rb
@@ -8,5 +8,36 @@ require 'pg'
 require 'pry'
 
 get('/') do
+  @divisions=Division.all()
   erb(:index)
+end
+
+post('/') do
+  name= params.fetch("name")
+  new_division = Division.new({:name => name})
+  new_division.save()
+  @divisions=Division.all()
+  erb(:index)
+end
+
+get('/division/:id') do
+  @division = Division.find(params.fetch("id").to_i())
+  erb(:division)
+end
+
+patch('/division/:id/update') do
+  @division = Division.find(params.fetch("id").to_i())
+  new_name = params.fetch("update_division")
+  @division.update({:name => new_name})
+  @divisions = Division.all()
+  erb(:index)
+end
+
+post('/employees') do
+  employee = params.fetch('name')
+  division_id = params.fetch('division_id').to_i()
+  @division = Division.find(division_id)
+  @employee = Employee.new({:name => employee, :division_id => division_id})
+  @employee.save()
+  erb(:division)
 end
